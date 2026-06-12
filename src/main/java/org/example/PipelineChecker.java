0a8fb4fca86633589f489a81698ad99bfbf82981
kastashenkova <astashenkova.katya@gmail.com>
2026-06-12 14:19:57 +0300

    update style

--- src/main/java/org/example/PipelineChecker.java
+++ src/main/java/org/example/PipelineChecker.java
@@ -1,12 +1,16 @@
 package org.example;
 
+/**
+ * Simple class for pipeline check.
+ * Final for inheritance ban.
+ */
 public final class PipelineChecker {
     static void main() {
         System.out.println("Pipeline is working");
     }
 
     /**
-     * Simple test method for pipeline check
+     * Simple test method for pipeline check.
      *
      * @return true if pipeline is working
      */
